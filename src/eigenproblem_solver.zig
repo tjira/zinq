94a01fbf1d2a72e7b959815d0bbef3acd4a56acf
Tomas Jira <tom.jira@gmail.com>
2025-09-03 17:00:33 +0200

    remove the redundant diagonalize function

--- src/eigenproblem_solver.zig
+++ src/eigenproblem_solver.zig
@@ -6,67 +6,18 @@ const real_matrix = @import("real_matrix.zig");
 
 const RealMatrix = real_matrix.RealMatrix;
 
-/// Solve the eigenproblem for a real symmetric system using the Jacobi method.
-pub fn diagonalizeJacobi(comptime T: type, A: *RealMatrix(T)) void {
-    std.debug.assert(A.rows == A.cols);
-
-    const n = A.rows;
-
-    if (n == 1) return;
-
-    const tol = 16 * std.math.floatEps(T) * A.frobeniusNorm();
-
-    while (A.offDiagonalFrobeniusNorm() > tol) for (0..n - 1) |i| for (i + 1..n) |j| {
-
-        const a = A.at(i, i); const b = A.at(j, j); const g = A.at(i, j);
-
-        if (@abs(g) < std.math.floatEps(T) * std.math.sqrt(a * a + b * b)) continue;
-
-        const tau = 0.5 * (b - a) / g;
-
-        const t = std.math.copysign(@as(T, 1), tau) / (@abs(tau) + std.math.hypot(1, tau));
-
-        const c = 1 / std.math.sqrt(1 + t * t); const s = t * c;
-
-        for (0..n) |k| {
-
-            if (k == i or k == j) continue;
-
-            const u = A.at(k, i);
-            const v = A.at(k, j);
-
-            const uk = c * u - s * v;
-            const vk = s * u + c * v;
-
-            A.ptr(k, i).* = uk; A.ptr(k, j).* = vk;
-            A.ptr(i, k).* = uk; A.ptr(j, k).* = vk;
-        }
-
-        A.ptr(i, i).* = a - t * g; A.ptr(i, j).* = 0;
-        A.ptr(j, j).* = b + t * g; A.ptr(j, i).* = 0;
-    };
-
-    for (0..n - 1) |_| for (0..n - 1) |j| if (A.at(j, j) > A.at(j + 1, j + 1)) {
-        std.mem.swap(T, A.ptr(j, j), A.ptr(j + 1, j + 1));
-    };
-}
-
 /// Diagonalize a real symmetric matrix A. The provided matrix is overwritten by the diagonal form.
 pub fn diagonalizeSymmetric(comptime T: type, A: *RealMatrix(T)) !void {
-    diagonalizeJacobi(T, A);
+    eigensystemJacobi(T, A, null, A.*);
 }
 
 /// Solve the eigenproblem for a real symmetric system using the Jacobi method.
-pub fn eigensystemJacobi(comptime T: type, J: *RealMatrix(T), C: *RealMatrix(T), A: RealMatrix(T)) void {
+pub fn eigensystemJacobi(comptime T: type, J: *RealMatrix(T), C: ?*RealMatrix(T), A: RealMatrix(T)) void {
     std.debug.assert(A.rows == A.cols);
-    std.debug.assert(J.rows == A.rows);
-    std.debug.assert(J.cols == A.cols);
-    std.debug.assert(C.rows == A.rows);
-    std.debug.assert(C.cols == A.cols);
 
-    const n = A.rows; @memcpy(J.data, A.data); C.identity();
+    const n = A.rows; @memcpy(J.data, A.data);
 
-    if (n == 1) return;
+    if (C != null) C.?.identity(); if (n == 1) return;
 
     const tol = 16 * std.math.floatEps(T) * A.frobeniusNorm();
 
@@ -99,21 +50,23 @@ pub fn eigensystemJacobi(comptime T: type, J: *RealMatrix(T), C: *RealMatrix(T),
         J.ptr(i, i).* = a - t * g; J.ptr(i, j).* = 0;
         J.ptr(j, j).* = b + t * g; J.ptr(j, i).* = 0;
 
-        for (0..n) |k| {
+        if (C != null) for (0..n) |k| {
 
-            const u = C.at(k, i);
-            const v = C.at(k, j);
+            const u = C.?.at(k, i);
+            const v = C.?.at(k, j);
 
-            C.ptr(k, i).* = c * u - s * v;
-            C.ptr(k, j).* = s * u + c * v;
-        }
+            C.?.ptr(k, i).* = c * u - s * v;
+            C.?.ptr(k, j).* = s * u + c * v;
+        };
     };
 
     for (0..n - 1) |_| for (0..n - 1) |j| if (J.at(j, j) > J.at(j + 1, j + 1)) {
 
         std.mem.swap(T, J.ptr(j, j), J.ptr(j + 1, j + 1));
 
-        for (0..n) |k| std.mem.swap(T, C.ptr(k, j), C.ptr(k, j + 1));
+        if (C != null) for (0..n) |k| {
+            std.mem.swap(T, C.?.ptr(k, j), C.?.ptr(k, j + 1));
+        };
     };
 }
 
