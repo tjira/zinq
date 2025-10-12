1d6a8130aec7b4f8d24f421e9bf9ef8de1f88727
Tomas Jira <tom.jira@gmail.com>
2025-10-12 09:02:17 +0200

    try to remove the jekyll-remote-theme plugin

--- docs/gems.rb
+++ docs/gems.rb
@@ -1,9 +1,4 @@
 source "https://rubygems.org"
 
 gem "erb", "4.0.3"
-
-gem "jekyll-include-cache", "0.2.1"
-gem "jekyll-remote-theme", "0.4.3"
-gem "jekyll-seo-tag", "2.8.0"
-
-gem "sass-embedded", "1.78.0" # https://github.com/just-the-docs/just-the-docs/pull/1548
+gem "just-the-docs", "0.10.1"
