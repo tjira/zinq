fb8f6e17ddef7481cf3fec4d62055d1d72a9029c
Tomas Jira <tom.jira@gmail.com>
2026-02-21 14:03:36 +0100

    small fix in setup.py

--- setup.py
+++ setup.py
@@ -35,13 +35,12 @@ class Bdist(setuptools.command.bdist_wheel.bdist_wheel):
         replacements = {
             "linux" : "manylinux_2_5",
             "macos" : "macosx_10_0",
-            "windows" : "win",
-            "aarch64" : "arm64"
+            "windows" : "win"
         }
 
         plat = f"{replacements.get(OS, OS)}_{replacements.get(ARCH, ARCH)}"
 
-        return "py3", "none", plat.replace("win_x86_64", "win_amd64").replace("manylinux_2_5_arm64", "manylinux_2_5_aarch64")
+        return "py3", "none", plat.replace("win_aarch64", "win_arm64").replace("win_x86_64", "win_amd64")
 
 class Build(setuptools.command.build_py.build_py):
     def run(self):
