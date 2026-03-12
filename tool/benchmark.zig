4a8704c3363d6af4bdeb581557783209ef6c2c21
Tomas Jira <tom.jira@gmail.com>
2026-03-12 10:36:36 +0100

    improve the benchmarking script

--- tool/benchmark.zig
+++ tool/benchmark.zig
@@ -4,6 +4,7 @@ const zinq = @import("zinq");
 
 const RealMatrix = zinq.real_matrix.RealMatrix;
 
+const eigensystemJacobi = zinq.eigenproblem_solver.eigensystemJacobi;
 const exportRealMatrix = zinq.device_write.exportRealMatrix;
 const mean = zinq.array_functions.mean;
 const mm = zinq.matrix_multiplication.mm;
@@ -11,11 +12,18 @@ const print = zinq.device_write.print;
 const sd = zinq.array_functions.sd;
 const timestamp = zinq.timestamp.timestamp;
 
-pub fn MmBenchStruct(comptime T: type) type {
+const SAMPLES = 100;
+
+pub fn EigensystemJacobiBenchStruct(comptime T: type) type {
     return struct {
         A: RealMatrix(T),
         B: RealMatrix(T),
         C: RealMatrix(T),
+
+        pub const name = "JACOBI EIGENSOLVER";
+
+        pub const min: usize =   2;
+        pub const max: usize = 100;
 
         pub fn init(allocator: std.mem.Allocator, size: usize) !@This() {
             return .{
@@ -31,27 +39,57 @@ pub fn MmBenchStruct(comptime T: type) type {
             self.C.deinit(allocator);
         }
 
-        pub fn randomize(self: *@This(), seed: usize) void {
-            self.A.randomize(seed);
-            self.B.randomize(seed);
+        pub fn run(self: *@This(), seed: usize) !u64 {
+            self.A.randomize(seed); try self.A.symmetrize();
+
+            var timer = try std.time.Timer.start();
+
+            try eigensystemJacobi(RealMatrix, T, &self.B, &self.C, self.A);
+
+            return timer.read();
+        }
+    };
+}
+
+pub fn MmBenchStruct(comptime T: type) type {
+    return struct {
+        A: RealMatrix(T),
+        B: RealMatrix(T),
+        C: RealMatrix(T),
+
+        pub const name = "MATRIX MULTIPLICATION";
+
+        pub const min: usize =   2;
+        pub const max: usize = 300;
+
+        pub fn init(allocator: std.mem.Allocator, size: usize) !@This() {
+            return .{
+                .A = try RealMatrix(T).init(size, size, allocator),
+                .B = try RealMatrix(T).init(size, size, allocator),
+                .C = try RealMatrix(T).init(size, size, allocator)
+            };
         }
 
-        pub fn run(self: *@This()) !u64 {
+        pub fn deinit(self: *@This(), allocator: std.mem.Allocator) void {
+            self.A.deinit(allocator);
+            self.B.deinit(allocator);
+            self.C.deinit(allocator);
+        }
+
+        pub fn run(self: *@This(), seed: usize) !u64 {
+            self.A.randomize(seed); self.B.randomize(seed);
+
             var timer = try std.time.Timer.start();
 
             try mm(T, &self.C, self.A, false, self.B, false);
 
             return timer.read();
         }
-
-        pub fn title(min_size: usize, max_size: usize, samples: usize) !void {
-            try print("\nMATRIX MULTIPLICATION BENCHMARK FROM {d}x{d} TO {d}x{d}, {d} SAMPLES EACH\n", .{min_size, min_size, max_size, max_size, samples});
-        }
     };
 }
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){}; const allocator = gpa.allocator();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){}; const allocator = gpa.allocator(); const T = f64;
 
     defer {
         if (gpa.deinit() == .leak) std.log.err("MEMORY LEAK DETECTED IN THE ALLOCATOR\n", .{});
@@ -65,7 +103,25 @@ pub fn main() !void {
         try print(", TIMESTAMP: {s}\n", .{ts});
     }
 
-    const mm_results = try benchmark(f64, MmBenchStruct(f64), 2, 300, 100, allocator); try exportRealMatrix(f64, "MM.mat", mm_results); mm_results.deinit(allocator);
+    const benchmarks = .{
+         EigensystemJacobiBenchStruct(T),
+         MmBenchStruct(T)
+    };
+
+    var timer = try std.time.Timer.start();
+
+    inline for (benchmarks) |bench_struct| {
+
+        const results = try benchmark(T, bench_struct, SAMPLES, allocator); defer results.deinit(allocator);
+
+        const fname = try std.fmt.allocPrint(allocator, "{s}.mat", .{bench_struct.name}); defer allocator.free(fname);
+
+        std.mem.replaceScalar(u8, fname, ' ', '_');
+
+        try exportRealMatrix(T, fname, results);
+    }
+
+    try print("\nBENCHMARKING COMPLETED IN {D}\n", .{timer.read()});
 }
 
 pub fn sortAndAnalyzeTimings(timings: []f64) !struct{min: u64, median: u64, mean: u64, max: u64, sd: u64} {
@@ -86,12 +142,12 @@ pub fn sortAndAnalyzeTimings(timings: []f64) !struct{min: u64, median: u64, mean
     };
 }
 
-pub fn benchmark(comptime T: type, bench_type: anytype, min_size: usize, max_size: usize, samples: usize, allocator: std.mem.Allocator) !RealMatrix(T) {
-    if (min_size < 2 or max_size < min_size or samples == 0) return error.InvalidInput;
+pub fn benchmark(comptime T: type, bench_type: anytype, samples: usize, allocator: std.mem.Allocator) !RealMatrix(T) {
+    if (bench_type.min < 2 or bench_type.max < bench_type.min or samples == 0) return error.InvalidInput;
 
-    var results = try RealMatrix(T).init(max_size - min_size + 1, 6, allocator); errdefer results.deinit(allocator);
+    var results = try RealMatrix(T).init(bench_type.max - bench_type.min + 1, 6, allocator); errdefer results.deinit(allocator);
 
-    try bench_type.title(min_size, max_size, samples);
+    try print("\n{s} BENCHMARK FROM {d}x{d} TO {d}x{d}, {d} SAMPLES EACH\n", .{bench_type.name, bench_type.min, bench_type.min, bench_type.max, bench_type.max, samples});
 
     try print("|---------|----------|-----------|---------|----------|---------|\n", .{});
     try print("|  SHAPE  | MIN TIME |MEDIAN TIME|MEAN TIME| MAX TIME | STD DEV |\n", .{});
@@ -99,27 +155,22 @@ pub fn benchmark(comptime T: type, bench_type: anytype, min_size: usize, max_siz
 
     var timings = try allocator.alloc(f64, samples); defer allocator.free(timings);
 
-    for (min_size..max_size + 1) |i| {
+    for (bench_type.min..bench_type.max + 1) |i| {
 
         var bench_struct = try bench_type.init(allocator, i); defer bench_struct.deinit(allocator);
 
-        for (0..samples) |j| {
-
-            bench_struct.randomize(j + 1);
-
-            timings[j] = @as(f64, @floatFromInt(try bench_struct.run()));
-        }
+        for (0..samples) |j| timings[j] = @as(f64, @floatFromInt(try bench_struct.run(j)));
 
         const analysis = try sortAndAnalyzeTimings(timings);
 
-        results.ptr(i - min_size, 0).* = @as(T, @floatFromInt(i              ));
-        results.ptr(i - min_size, 1).* = @as(T, @floatFromInt(analysis.min   ));
-        results.ptr(i - min_size, 2).* = @as(T, @floatFromInt(analysis.median));
-        results.ptr(i - min_size, 3).* = @as(T, @floatFromInt(analysis.mean  ));
-        results.ptr(i - min_size, 4).* = @as(T, @floatFromInt(analysis.max   ));
-        results.ptr(i - min_size, 5).* = @as(T, @floatFromInt(analysis.sd    ));
+        results.ptr(i - bench_type.min, 0).* = @as(T, @floatFromInt(i              ));
+        results.ptr(i - bench_type.min, 1).* = @as(T, @floatFromInt(analysis.min   ));
+        results.ptr(i - bench_type.min, 2).* = @as(T, @floatFromInt(analysis.median));
+        results.ptr(i - bench_type.min, 3).* = @as(T, @floatFromInt(analysis.mean  ));
+        results.ptr(i - bench_type.min, 4).* = @as(T, @floatFromInt(analysis.max   ));
+        results.ptr(i - bench_type.min, 5).* = @as(T, @floatFromInt(analysis.sd    ));
 
-        for (1..6) |col| results.ptr(i - min_size, col).* /= std.time.ns_per_ms;
+        for (1..6) |col| results.ptr(i - bench_type.min, col).* /= std.time.ns_per_ms;
 
         const print_payload = .{.value = i, .min = analysis.min, .median = analysis.median, .mean = analysis.mean, .max = analysis.max, .sd = analysis.sd};
 
