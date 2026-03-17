13bbcab785668986082281cf0adeed73608c3642
Tomas Jira <tom.jira@gmail.com>
2026-03-17 11:41:14 +0100

    modify the profiling script

--- script/profile_binary.sh
+++ script/profile_binary.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-    echo "USAGE: $0 <COMMAND>" && exit 1
-fi
+perf record -g -- "$@"
 
-valgrind --callgrind-out-file=callgrind.out --tool=callgrind $1 && gprof2dot -e 1 -f callgrind -n 5 -o profile.dot callgrind.out && dot -T pdf profile.dot -o profile.pdf
+perf script | gprof2dot -f perf | dot -T pdf -o profile.pdf
+
+rm -f perf.data
