f20db557db42cb0fdf2b1acac2c9db869a05f2b5
Tomas Jira <tom.jira@gmail.com>
2026-04-01 13:34:10 +0200

    update Jahn-Teller entropy archive example

--- archive/jahn_teller_ci_entropy.sh
+++ archive/jahn_teller_ci_entropy.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
-MAX_D=100
+D=20; MAX_CID=19
 
-echo "$MAX_D 2" > JAHN_TELLER_CI_ENTROPY.mat
+echo "$MAX_CID 2" > JAHN_TELLER_CI_ENTROPY.mat
 
-for D in $(seq 2 $MAX_D); do
+for CS in $(seq 0 $MAX_CID); do
 
-    cp jahn_teller_ci_entropy.json input.json
+    CID=$(($D-$CS-1)); cp jahn_teller_ci_entropy.json input.json
 
-    jq --argjson d "$D" '.zinq[0].options.initial_conditions.model.momentum_mean = [range($d) | 0   ]   ' input.json > tmp.json && mv tmp.json input.json
-    jq --argjson d "$D" '.zinq[0].options.initial_conditions.model.position_mean = [range($d) | 1e-5]   ' input.json > tmp.json && mv tmp.json input.json
-    jq --argjson d "$D" '.zinq[0].options.initial_conditions.model.gamma_mean    = [range($d) | 0   ]   ' input.json > tmp.json && mv tmp.json input.json
-    jq --argjson d "$D" '.zinq[0].options.initial_conditions.model.mass          = [range($d) | 1   ]   ' input.json > tmp.json && mv tmp.json input.json
-    jq --argjson d "$D" '.zinq[0].options.potential.jahn_teller.d                = $d                   ' input.json > tmp.json && mv tmp.json input.json
-    jq --argjson d "$D" '.zinq[0].options.write.position_mean                    = "POSITION_\($d)D.mat"' input.json > tmp.json && mv tmp.json input.json
-    jq --argjson d "$D" '.zinq[0].options.write.state_potential_energy_mean      = "PES_\($d)D.mat"     ' input.json > tmp.json && mv tmp.json input.json
+    jq --argjson d "$D" '.zinq[0].options.initial_conditions.model.momentum_mean = [range($d) | 0   ]' input.json > tmp.json && mv tmp.json input.json
+    jq --argjson d "$D" '.zinq[0].options.initial_conditions.model.position_mean = [range($d) | 1e-5]' input.json > tmp.json && mv tmp.json input.json
+    jq --argjson d "$D" '.zinq[0].options.initial_conditions.model.gamma_mean    = [range($d) | 0   ]' input.json > tmp.json && mv tmp.json input.json
+    jq --argjson d "$D" '.zinq[0].options.initial_conditions.model.mass          = [range($d) | 1   ]' input.json > tmp.json && mv tmp.json input.json
+    jq --argjson d "$D" '.zinq[0].options.potential.jahn_teller.d                = $d                ' input.json > tmp.json && mv tmp.json input.json
+    jq --argjson d "$D" '.zinq[0].options.potential.jahn_teller.abs_coupling     = true              ' input.json > tmp.json && mv tmp.json input.json
+
+    jq --argjson cs "$CS" '.zinq[0].options.potential.jahn_teller.coupling_sum = $cs' input.json > tmp.json && mv tmp.json input.json
+
+    jq --argjson cid "$CID" '.zinq[0].options.write.position_mean                = "POSITION_\($cid)D.mat"' input.json > tmp.json && mv tmp.json input.json
+    jq --argjson cid "$CID" '.zinq[0].options.write.state_potential_energy_mean  = "PES_\($cid)D.mat"     ' input.json > tmp.json && mv tmp.json input.json
 
     ENTROPY=$(zinq | grep SCHLITTER | awk '{print $6}')
 
-    printf "%03d %12.8f\n" "$D" "$ENTROPY" >> JAHN_TELLER_CI_ENTROPY.mat
+    printf "%03d %12.8f\n" "$CID" "$ENTROPY" >> JAHN_TELLER_CI_ENTROPY.mat
 
     tail -n 1 JAHN_TELLER_CI_ENTROPY.mat
 done
 
 rm input.json
 
-plot JAHN_TELLER_CI_ENTROPY.mat POSITION_4D.mat:2,3 --figsize 6 16 --subplots 121 122 122 122 122 --legends 1,2 "First CI Coordinate" "Second CI Coordinate" --title "Schlitter Entropy of CI as a Function of Dimension" "CI Coordinates of 4D Model as a function of Time" --xlabel "Dimension" "Time (a.u.)" --ylabel "Schlitter Entropy of CI (J/K/Mol)" "Position (a.u.)" --output JAHN_TELLER_CI.png
+plot JAHN_TELLER_CI_ENTROPY.mat --title "Seam Entropy as a Function of Dimension on a ${D}D Potential" --xlabel "Seam Dimension" --ylabel "Schlitter Entropy of CI (J/K/Mol)" --output JAHN_TELLER_CI.png
--- src/jahn_teller_potential.zig
+++ src/jahn_teller_potential.zig
@@ -11,22 +11,22 @@ const RealVector = real_vector.RealVector;
 /// Struct holding parameters for the Jahn-Teller potential.
 pub fn JahnTellerPotential(comptime T: type) type {
     return struct {
-        k: T = 1, g: T = 1, d: usize = 2,
+        k: T = 1, g: T = 1, d: usize = 2, abs_coupling: bool = false, coupling_sum: usize = 1,
 
         /// Diabatic potential matrix evaluator.
         pub fn evaluateDiabatic(self: @This(), U: *RealMatrix(T), position: RealVector(T), time: T) !void {
-            U.ptr(0, 0).* = self.evaluateDiabaticElementComptime(0, 0, position, time);
-            U.ptr(0, 1).* = self.evaluateDiabaticElementComptime(0, 1, position, time);
-            U.ptr(1, 0).* = self.evaluateDiabaticElementComptime(1, 0, position, time);
-            U.ptr(1, 1).* = self.evaluateDiabaticElementComptime(1, 1, position, time);
+            U.ptr(0, 0).* = try self.evaluateDiabaticElementComptime(0, 0, position, time);
+            U.ptr(0, 1).* = try self.evaluateDiabaticElementComptime(0, 1, position, time);
+            U.ptr(1, 0).* = try self.evaluateDiabaticElementComptime(1, 0, position, time);
+            U.ptr(1, 1).* = try self.evaluateDiabaticElementComptime(1, 1, position, time);
         }
 
         /// Diabatic potential matrix element evaluator.
         pub fn evaluateDiabaticElement(self: @This(), i: usize, j: usize, position: RealVector(T), time: T) !T {
             return switch (i + j) {
-                0 => self.evaluateDiabaticElementComptime(0, 0, position, time),
-                1 => self.evaluateDiabaticElementComptime(0, 1, position, time),
-                2 => self.evaluateDiabaticElementComptime(1, 1, position, time),
+                0 => try self.evaluateDiabaticElementComptime(0, 0, position, time),
+                1 => try self.evaluateDiabaticElementComptime(0, 1, position, time),
+                2 => try self.evaluateDiabaticElementComptime(1, 1, position, time),
                 else => {
 
                     std.log.err("INVALID INDEX ({d}, {d}) WHEN EVALUATING DIABATIC MATRIX ELEMENT, THE POTENTIAL MATRIX IS {d}X{d}", .{i, j, 2, 2});
@@ -37,7 +37,7 @@ pub fn JahnTellerPotential(comptime T: type) type {
         }
 
         /// Comptime potential matrix element evaluator.
-        pub fn evaluateDiabaticElementComptime(self: @This(), comptime i: usize, comptime j: usize, position: RealVector(T), _: T) T {
+        pub fn evaluateDiabaticElementComptime(self: @This(), comptime i: usize, comptime j: usize, position: RealVector(T), _: T) !T {
             return switch (i + j) {
                 0 => {
 
@@ -45,7 +45,21 @@ pub fn JahnTellerPotential(comptime T: type) type {
 
                     return self.g * position.at(0) + 0.5 * self.k * sumsq;
                 },
-                1 => self.g * position.at(1),
+                1 => {
+
+                    if (self.coupling_sum > position.len - 1) {
+
+                        std.log.err("COUPLING SUM HAS TO BE LESS THAN OR EQUAL TO THE NUMBER OF NUCLEAR COORDINATES MINUS ONE", .{});
+
+                        return error.InputError;
+                    }
+
+                    var sum: T = 0;
+
+                    for (0..self.coupling_sum) |k| sum += if (self.abs_coupling) @abs(position.at(1 + k)) else position.at(1 + k);
+
+                    return self.g * sum;
+                },
                 2 => {
                     
                     var sumsq: T = 0; for (position.data) |q| sumsq += q * q;
