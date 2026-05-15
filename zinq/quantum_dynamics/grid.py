small fix for product calculation

zinq/quantum_dynamics/grid.py
@@ -25,7 +25,7 @@ class Grid:
 
         self.position = self._pos_grid(np.array(limits), npoint)
         self.momentum = self._mom_grid(np.array(limits), npoint)
-        self.measure = np.prod([np.ptp(axis) / (npoint - 1) for axis in self.position])
+        self.measure = np.array([np.ptp(axis) / (npoint - 1) for axis in self.position]).prod()
 
     def _mom_grid(self, limits: np.ndarray, npoint: int) -> list[np.ndarray]:
         grids = []