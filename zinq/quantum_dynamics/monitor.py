afeb34e4a6ab1577f1e29860f411bf3d85dece35
Tomas Jira <tom.jira@gmail.com>
2026-05-22 09:33:04 +0200

    add spectrum calculation

--- zinq/quantum_dynamics/monitor.py
+++ zinq/quantum_dynamics/monitor.py
@@ -27,7 +27,7 @@ class Monitor:
         if self.opt.write is None: return
 
         for field, path in self.opt.write.model_dump().items():
-            if not path or field == "spectrum": continue
+            if not path: continue
 
             data = self._format_data(field, dt); header = f"{data.shape[0]} {data.shape[1]}"
 
@@ -51,7 +51,7 @@ class Monitor:
             "norm"
         }
 
-        return {k: k in active for k in set(Result.__annotations__) | active}
+        return {k: k in active for k in (set(Result.__annotations__) | active) - {"spectrum"}}
     
     def record(self, i: int, wfn: Wavefunction, wfn_0: Wavefunction | None) -> None:
         wfn_adia, to_calc = wfn.to_adia(self.H) if self.opt.adiabatic else wfn, self._to_calc(i)
@@ -76,6 +76,9 @@ class Monitor:
             self._print_step(SimpleNamespace(**obs), i)
 
     def _format_data(self, name: str, dt: float) -> np.ndarray:
+        if name == "spectrum":
+            return self._get_spectrum(np.array(self.history["autocorrelation"]))
+
         def normal_data(data) -> np.ndarray:
             return data.reshape(data.shape[0], -1)
 
@@ -97,6 +100,31 @@ class Monitor:
 
         return np.column_stack((*funcs.get(name, (normal_data, normal_grid))[1](data), data))
     
+    def _get_spectrum(self, acf: np.ndarray) -> np.ndarray:
+        dt, times =self.opt.time_step, np.arange(-len(acf) + 1, len(acf)) * self.opt.time_step
+
+        def get_omega(acf) -> np.ndarray:
+            return np.fft.fftshift(2 * np.pi * np.fft.fftfreq(len(acf), dt))
+
+        def get_spectrum(acf) -> np.ndarray:
+            return np.real(np.fft.fftshift(np.fft.fft(np.fft.ifftshift(np.conj(acf))) * dt))
+
+        def get_tau(acf) -> float:
+            return -np.log(1e-4) / (len(acf) * dt)**2
+
+        def damp_acf(acf, tau: float) -> np.ndarray:
+            return acf * np.exp(-tau * times**2)
+
+        def pad_acf(acf) -> np.ndarray:
+            return np.pad(acf, 2 * [10 * len(acf)], mode="constant")
+
+        def symmetrize_acf(acf) -> np.ndarray:
+            return np.concatenate((np.conj(acf[1:][::-1]), acf))
+
+        acf = pad_acf(damp_acf(symmetrize_acf(acf), get_tau(acf)))
+
+        return np.column_stack((get_omega(acf), get_spectrum(acf)))
+
     def _is_log(self, i: int) -> bool:
         iters, interval = self.opt.iterations, self.opt.log_interval
 
