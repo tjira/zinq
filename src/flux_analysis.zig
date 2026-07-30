3bf2e1c74f335738924a15aa8ee40c2ccdb2e3c9
Tomas Jira <tom.jira@gmail.com>
2026-07-30 15:20:24 +0200

    add a 3D cross section mapping

--- src/flux_analysis.zig
+++ src/flux_analysis.zig
@@ -233,10 +233,16 @@ pub fn FluxAnalysisContext(comptime T: type) type {
 
                             if (!in_bounds) continue;
 
-                            const val = flux_acc.at(row, i).conjugate().mul(temp_phi[i]).im;
+                            const val, var weight: T = .{ flux_acc.at(row, i).conjugate().mul(temp_phi[i]).im, 0 };
 
-                            if ((i / s_d) % npoint == n_max) sum += factor * val;
-                            if ((i / s_d) % npoint == n_min) sum -= factor * val;
+                            for (0..r.ncol()) |k| if (k != d) {
+                                weight += r.at(i, k) * r.at(i, k);
+                            };
+
+                            weight = if (r.ncol() > 1) std.math.sqrt(weight) else 1;
+
+                            if ((i / s_d) % npoint == n_max) sum += factor * val * weight;
+                            if ((i / s_d) % npoint == n_min) sum -= factor * val * weight;
                         }
 
                         prob_matrix.ptr(ei, f).* += sum;
